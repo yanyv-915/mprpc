816734611975b46f99617206ec05296f885e4097
liuyixuan <3467286803@qq.com>
2026-04-04 00:43:19 +0800

    完善了类的模板化应用

--- day1/include/cache/Vector.h
+++ day1/include/cache/Vector.h
@@ -2,16 +2,39 @@
 #include"../core/IVectorData.h"
 #include<vector>
 #include"../network/Protocol.h"
-class FloatVector : public IVectorData{
+
+#include<assert.h>
+
+// --- 1. 先定义 Traits（类型映射），必须放在类外面 ---
+template<typename T> struct TypeToTag;
+
+template<> struct TypeToTag<float>   { static constexpr DataType value = DataType::FLOAT32; };
+template<> struct TypeToTag<int16_t> { static constexpr DataType value = DataType::INT16;   };
+template<> struct TypeToTag<uint8_t> { static constexpr DataType value = DataType::BINARY;   };
+template<typename T>
+class VectorDataImpl : public IVectorData{
 private:
-    std::vector<float> data;
+    std::vector<T> _data;
 public:
-    FloatVector(std::vector<float>&& d):data(std::move(d)) {};
-    FloatVector(uint32_t dim){
-        data.resize(dim,0);
+    VectorDataImpl(std::vector<T>&& d):_data(std::move(d)){}
+    VectorDataImpl(uint32_t dim){_data.resize(dim,0.0f);}
+    float compute_l2(const IVectorData* other) const override {
+        assert(other->getTypeTag() == this->getTypeTag());
+        const T* p2 = static_cast<const T*>(other->getRawPtr());
+        const T* p1 = _data.data();
+        size_t n=_data.size();
+
+        float sum=0.0f;
+        for(size_t i=0;i<n;i++){
+            float diff = static_cast<float> (p1[i]) - static_cast<float>(p2[i]);
+            sum += diff*diff;
+        }
+        return sum;
     }
-    size_t getSize() const override {return data.size()* sizeof(float);}
-    const void* getRawPtr() const override {return data.data();}
-    DataType getTypeTag() const override {return DataType::FLOAT32;}
-    uint32_t dim() const override {return data.size();}
+    const void* getRawPtr() const override {return _data.data();}
+    size_t getSize() const override{return _data.size()*sizeof(T);}
+    uint32_t dim() const override {return _data.size();}
+    DataType getTypeTag() const override {
+        return TypeToTag<T>::value;
+    }
 };
--- day1/include/utils/VectorFactory.h
+++ day1/include/utils/VectorFactory.h
@@ -4,15 +4,21 @@
 #include"../cache/Vector.h"
 
 #include<memory>
+
+using FloatVectorData = VectorDataImpl<float>;
+using Int16VectorData = VectorDataImpl<int16_t>;
+
 class VectorFactoy{
 public:
     static std::shared_ptr<IVectorData> create(DataType typeTag,uint32_t dim){
         switch (typeTag)
         {
         case DataType::FLOAT32:
-            return std::make_shared<FloatVector>(dim);
+            return std::make_shared<FloatVectorData>(dim);
             break;
-        
+        case DataType::INT16:
+            return std::make_shared<Int16VectorData>(dim);
+            break;
         default:
             return nullptr;
         }
